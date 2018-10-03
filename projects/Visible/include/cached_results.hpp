f1a02adffd097617d02cfdce335964eb8c7a5ec2
Arman Garakani <arman.garakani@leoilab.com>
2018-10-03 14:43:55 -0400

    adding cached_results.hpp Fixing code registration
    Former-commit-id: 2897eb3938dc54b4dd15585220669d6aba57b41a

---
+++ projects/Visible/include/cached_results.hpp
@@ -0,0 +1,76 @@
+#ifndef __CACHED_REGISTRY__
+#define __CACHED_REGISTRY__
+
+
+#include <map>
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <vector>
+#include <sstream>
+#include <typeindex>
+#include <map>
+#include <future>
+#include "core/singleton.hpp"
+#include "async_producer.h"
+
+
+
+
+
+// The base type that is stored in the collection.
+struct algo_func_t {
+    virtual ~algo_func_t() = default;
+};
+
+// Derived representing a callback with its associated argument(s)
+template<typename ...A>
+struct algo_cb_t : public algo_func_t {
+    using cb = std::function<timed_double_t(A...)>;
+    cb callback;
+    algo_cb_t(cb p_callback) : callback(p_callback) {}
+};
+
+class cached_results : public SingletonLight <cached_results>
+{
+public:
+    typedef std::map<std::type_index, std::unique_ptr<algo_func_t>> callbacks_t;
+    
+        algo_library ()
+    {
+        
+    }
+    
+    template<typename T>
+    std::type_index add (std::unique_ptr<T>& algoFn)
+    {
+        std::type_index index1(typeid(algoFn));
+        m_algo_callbacks.insert(callbacks_t::value_type(index1, std::move(algoFn)));
+        return index1;
+    }
+    
+    template<typename ...A>
+    bool call(std::type_index index, A&& ... args)
+    {
+        using func_t = algo_cb_t<A...>;
+        using cb_t = std::function<bool(A...)>;
+        const algo_func_t& base = *m_algo_callbacks[index];
+        const cb_t& fun = static_cast<const func_t&>(base).callback;
+        return fun(std::forward<A>(args)...);
+    }
+    
+    
+    void clear ()
+    {
+        m_algo_callbacks.clear ();
+    }
+    
+private:
+    
+    callbacks_t m_algo_callbacks;
+
+};
+
+
+
+#endif
