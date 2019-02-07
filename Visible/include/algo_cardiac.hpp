1e628227a8ed0508404a8a46ac9decb61a10b344
Arman Garakani <arman@nabokov.local>
2019-02-06 17:00:36 -0800

    1. Added Utility to save all images in a channel to a directory
    Former-commit-id: 4bcab4c555fc0c26fec9aaa235bacc24b16c69f2

--- Visible/include/algo_cardiac.hpp
+++ Visible/include/algo_cardiac.hpp
@@ -101,38 +101,57 @@ struct IntensityStatisticsPartialRunner
 
 
 
+// For base classing
+class OCVImageWriterBase : public base_signaler
+{
+    virtual std::string getName () const { return "OCVImageWriterBase"; }
+};
 
-class OCVImageWriter
+
+class OCVImageWriter : public OCVImageWriterBase
 {
 public:
     typedef std::function<std::string(const uint32_t& index, uint32_t field_width, char fill_char)> file_naming_fn_t;
     
-    OCVImageWriter(const std::string& dir_fqfn,
-                   const std::string& image_name = "image",
+    typedef void (writer_info_delegate) (int,bool);
+    
+    OCVImageWriter(const std::string& image_name = "image",
                    const file_naming_fn_t namer = OCVImageWriter::default_namer,
                    const std::string& file_sep = "/"):
-    m_fqfn(std::move(dir_fqfn)), m_name(std::move(image_name)), m_sep(std::move(file_sep)),m_namer(namer)
+        m_name(std::move(image_name)), m_sep(std::move(file_sep)),m_namer(namer)
     {
-        m_valid = boost::filesystem::exists( dir_fqfn );
-        m_valid = boost::filesystem::is_directory( dir_fqfn );
-        m_name = m_fqfn + m_sep +m_name;
+        // Signals we provide
+        signal_write_info = createSignal<OCVImageWriter::writer_info_delegate>();
     }
+ 
     typedef std::vector<roiWindow<P8U>> channel_images_t;
-    void operator()(channel_images_t& channel)
+    void operator()(const std::string& dir_fqfn, channel_images_t& channel)
     {
+        m_fqfn = std::move(dir_fqfn);
+        m_valid = boost::filesystem::exists( m_fqfn );
+        m_valid = m_valid && boost::filesystem::is_directory( m_fqfn );
+        if(!m_valid){
+            if (signal_write_info && signal_write_info->num_slots() > 0)
+                signal_write_info->operator()(0,true);
+            return;
+        }
+        m_name = m_fqfn + m_sep +m_name;
+        
         if (channel.empty()) return;
         int fw = 1 + std::log10(channel.size());
         std::lock_guard<std::mutex> guard (m_mutex);
         
         uint32_t count = 0;
+        uint32_t total = static_cast<uint32_t>(channel.size());
         vector<roiWindow<P8U> >::const_iterator vitr = channel.begin();
         do
         {
             cv::Mat mat(vitr->height(), vitr->width(), CV_8UC(1), vitr->rowPointer(0), 512);
             std::string fn = m_name +  m_namer(count++, fw, '0') + ".png";
             cv::imwrite(fn, mat);
-            std::cout << fn << std::endl;
-            
+            // Signal to listeners
+            if (signal_write_info && signal_write_info->num_slots() > 0)
+                signal_write_info->operator()(count-1,count == total);
         }
         while (++vitr != channel.end());
     }
@@ -144,7 +163,13 @@ public:
         s = ss.str();
         return s;
     }
+    
+    
+protected:
+    boost::signals2::signal<OCVImageWriter::writer_info_delegate>* signal_write_info;
+    
 private:
+    
     mutable std::mutex m_mutex;
     bool m_valid;
     std::string m_fqfn;
