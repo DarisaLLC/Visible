d995a9a885eccde256b3c8b4f44a1e8421e9feb4
arman <gmnamra@gmail.com>
2019-05-05 22:24:29 -0700

    1. Discovered bug in Affine Rectangle Draw
    Former-commit-id: e39bf99b0a5ec17be152a9fb9e6cc60cd4555049

--- svl/include/cinder_cv/affine_rectangle.h
+++ svl/include/cinder_cv/affine_rectangle.h
@@ -55,7 +55,6 @@ public:
     {
         mCornersWorld.resize(4);
         mCornersWindow.resize(4);
-        mCornersCv.resize(4);
     }
     
     affineRectangle (const Rectf& ar) : Rectf(ar)
@@ -63,7 +62,6 @@ public:
         mScale = vec2(1);
         mCornersWorld.resize(4);
         mCornersWindow.resize(4);
-        mCornersCv.resize(4);
     }
     
     ~affineRectangle () {}
@@ -122,11 +120,12 @@ public:
         mCornersWorld[1]=mArea->getUpperRight();
         mCornersWorld[2]=mArea->getLowerRight();
         mCornersWorld[3]=mArea->getLowerLeft();
+        mWorldCenter = mArea->getCenter();
         
         return mCornersWorld;
     }
     
-    std::vector<Point2f>& windowCorners (Point2f& center)
+    cv::RotatedRect get_rotated_rect ()
     {
         Point2f ctr (0.0f,0.0f);
         mCornersWindow.resize(4);
@@ -135,97 +134,31 @@ public:
             vec4 world = matrix() * vec4( mCornersWorld[i], 0, 1 );
             vec2 window = gl::worldToWindowCoord( vec3( world ) );
             mCornersWindow[i]=toOcv(window);
-            ctr.x += mCornersWorld[i].x;
-            ctr.y += mCornersWorld[i].y;
         }
-        center.x = ctr.x / 4.0f;
-        center.y = ctr.y / 4.0f;
+        {
+            vec4 world = matrix() * vec4( mWorldCenter, 0, 1 );
+            vec2 window = gl::worldToWindowCoord( vec3( world ) );
+            mWindowCenter=toOcv(window);
+        }
         
-        return mCornersWindow;
-    }
-    
-#ifdef NOT_YET
-    cv::Mat rectify_image(cv::Mat& image) {
-        Point2f center;
-        std::vector<cv::Point2f> ipts = windowCorners(center);
-        auto rr = cv::minAreaRect(ipts);
+        auto rr = cv::minAreaRect(mCornersWindow);
         if (rr.angle < -45.0){
             rr.angle += 90.0;
             auto sz = rr.size;
             rr.size.width = sz.height;
             rr.size.height = sz.width;
         }
+   //     rr.points(mCornersWindow.data());
         
-        std::vector<cv::Point2f> pts(4);
-        rr.points(pts.data());
-        
-        auto _scale = [](const cv::Point2f& pt, float mul){
-            cv::Point2f rr (pt.x * mul, pt.y*mul);
-            return rr;
-        };
-        
-        cv::Point2f tl = _scale(pts[0], 0.5f);
-        cv::Point2f tr = _scale(pts[1], 0.5f);
-        cv::Point2f br = _scale(pts[2], 0.5f);
-        cv::Point2f bl = _scale(pts[3], 0.5f);
-        
-        // compute the width of the new image, which will be the
-        // maximum distance between bottom - right and bottom - left
-        // x - coordiates or the top - right and top - left x - coordinates
-        float widthA = cv::norm(br - bl);
-        float widthB = cv::norm(tr - tl);
-        int maxWidth = std::max(int(widthA), int(widthB));
-        
-        // compute the height of the new image, which will be the
-        // maximum distance between the top - right and bottom - right
-        // y - coordinates or the top - left and bottom - left y - coordinates
-        float heightA = cv::norm(tr - br);
-        float heightB = cv::norm(tl - bl);
-        int maxHeight = std::max(int(heightA), int(heightB));
-        
-        // now that we have the dimensions of the new image, construct
-        // the set of destination points to obtain a "birds eye view",
-        // (i.e. top-down view) of the image, again specifying points
-        // in the top - left, top - right, bottom - right, and bottom - left
-        // order
-        std::vector<cv::Point2f> dst(4);
-        dst[0] = cv::Point2f(0, maxHeight - 1);
-        dst[3] = cv::Point2f(maxWidth - 1, maxHeight - 1);
-        dst[2] = cv::Point2f(maxWidth - 1, 0);
-        dst[1] = cv::Point2f(0, 0);
-        
-        // compute the perspective transform matrix and then apply it
-        cv::Mat M = cv::getPerspectiveTransform(pts, dst);
-        cv::Mat warped;
-        cv::warpPerspective(image, warped, M, cv::Size(maxWidth, maxHeight));
-        
-        // return the warped image
-        return warped;
-    }
-#endif
-    
-    cv::RotatedRect get_rotated_rect ()
-    {
-        Point2f center;
-        std::vector<Point2f>& corners = windowCorners (center);
-        auto rr = cv::minAreaRect(corners);
-        if (rr.angle < -45.0){
-            rr.angle += 90.0;
-            auto sz = rr.size;
-            rr.size.width = sz.height;
-            rr.size.height = sz.width;
-        }
-        rr.points(corners.data());
-        
-        float lr_dist = glm::distance(fromOcv(corners[2]), fromOcv(corners[1]));
-        float td_dist = glm::distance(fromOcv(corners[1]), fromOcv(corners[0]));
-        Point2f minor_axis = (lr_dist < td_dist) ? corners[2] - corners[1] : corners[0] - corners[1];
+        float lr_dist = glm::distance(fromOcv(mCornersWindow[2]), fromOcv(mCornersWindow[1]));
+        float td_dist = glm::distance(fromOcv(mCornersWindow[1]), fromOcv(mCornersWindow[0]));
+        Point2f minor_axis = (lr_dist < td_dist) ? mCornersWindow[2] - mCornersWindow[1] : mCornersWindow[0] - mCornersWindow[1];
         mCvMinorAxis = toDegrees (atan2(-minor_axis.y , minor_axis.x));
 
         cv::Size rrsize (std::min(lr_dist,td_dist), std::max(lr_dist, td_dist));
-        float dirty_angle = mCvMinorAxis;
-        return cv::RotatedRect(toOcv(position()), rrsize, dirty_angle);
+        return cv::RotatedRect(mWindowCenter, rrsize, mCvMinorAxis);
 
+        
     }
     
     
@@ -255,18 +188,8 @@ private:
     
     void compute2DRectCorners(const cv::RotatedRect rect, std::vector<cv::Point2f> & corners) const
     {
-        std::cout << "cv: " << rect.angle << std::endl;
-        
-        float a = (float) sin(toRadians(rect.angle)) * 0.5f;
-        float b = (float) cos(toRadians(rect.angle)) * 0.5f;
-        corners[0].x = rect.center.x - a * rect.size.height - b * rect.size.width;
-        corners[0].y = rect.center.y + b * rect.size.height - a * rect.size.width;
-        corners[1].x = rect.center.x + a * rect.size.height - b * rect.size.width;
-        corners[1].y = rect.center.y - b * rect.size.height - a * rect.size.width;
-        corners[2].x = 2 * rect.center.x - corners[0].x;
-        corners[2].y = 2 * rect.center.y - corners[0].y;
-        corners[3].x = 2 * rect.center.x - corners[1].x;
-        corners[3].y = 2 * rect.center.y - corners[1].y;
+        corners.resize(4);
+        return rect.points(corners.data());
     }
     
     Point2f map_point(Point2f pt) const
@@ -285,9 +208,10 @@ private:
     vec2   mScale;
     quat   mRotation;
     mutable cv::Mat mXformMat;
-    mutable std::vector<Point2f> mCornersCv;
     mutable std::vector<vec2> mCornersWorld;
     mutable std::vector<Point2f> mCornersWindow;
+    mutable Point2f mWindowCenter;
+    mutable vec2 mWorldCenter;
     mutable float mCvMinorAxis;
 };
 
