c1f645dfade6ac935d1e93abbd83db7a5ceae4a2
arman <gmnamra@gmail.com>
2019-05-14 13:42:48 -0700

    adding cv rotatedRect interface
    Former-commit-id: c1533d17d895633c5f565901a51a4572b26153f2

--- svl/include/cinder_cv/affine_rectangle.h
+++ svl/include/cinder_cv/affine_rectangle.h
@@ -23,132 +23,68 @@ using namespace ci;
 using namespace std;
 using namespace cv;
 
+
 //! A rectangle that can be transformed.
-/*
- struct EditableRect
- {
- Rectf  area;
- vec2   position;
- vec2   scale;
- quat   rotation;
- 
- EditableRect() : scale( 1 ) {}
- ~EditableRect() {}
- 
- //! Returns the rectangle's model matrix.
- mat4  matrix()
- {
- mat4 m = glm::translate( vec3( position, 0 ) );
- m *= glm::toMat4( rotation );
- m *= glm::scale( vec3( scale, 1 ) );
- m *= glm::translate( vec3( -area.getSize() * 0.5f, 0 ) );
- 
- return m;
- }
- 
- };
- 
- 
- OpenCv RotatedRect:
- The rotation angle in a clockwise direction. When the angle is 0, 90, 180, 270 etc., the rectangle becomes an up-right rectangle.
- 
- 
- */
-
-class affineRectangle : private Rectf
-{
-public:
-    affineRectangle ();
-    affineRectangle (const Rectf& ar);
-    ~affineRectangle () {}
+struct EditableRect {
+    Area  area;
+    vec2  position;
+    vec2  scale;
+    quat  rotation;
     
-    affineRectangle(const affineRectangle& other);
-    affineRectangle& operator=(const affineRectangle& rhs);
+    EditableRect() : scale( 1 ) {}
+    ~EditableRect() {}
     
-    void update () const;
-    
-    // Getters
-    const Rectf&  area () const;
-    const vec2&   position () const;
-    Rectf  norm_area () const { return screenToNormal().map(area()); }
-    vec2   norm_position () const { return screenToNormal().map(position()); }
-    
-    const vec2&   scale () const;
-    const quat   rotation ()const;
     //! Returns the rectangle's model matrix.
-    const mat4  &getWorldTransform() const;
-    float degrees () const;
-    float radians () const;
-    const cv::RotatedRect& rotated_rect () const;
-    const RectMapping&    screenToNormal () const;
-    
-    
-    // Modifiers Call update
-    void   scale (const vec2& ns);
-    void   position (const vec2& np);
-    void rotation (const quat& rq);
-    void resize (const int width, const int height);
-    void inflate (const vec2 trim);
-    
-    //The order is bottomLeft, topLeft, topRight, bottomRight.
-    // return in tl,tr,br,bl
-    // @note Check if this makes sense in all cases 
-    const std::vector<vec2>& worldCorners () const;
-    const std::vector<Point2f>& windowCorners () const;
-    
-    vec2 scale_map (const vec2& pt)
+    mat4  matrix()
     {
-        return (vec2 (pt[0]*mScale[0],pt[1]*mScale[1]));
-    }
-    
-    
-    static void compute2DRectCorners(const cv::RotatedRect rect, std::vector<cv::Point2f> & corners)
-    {
-        corners.resize(4);
-        return rect.points(corners.data());
-    }
-    
-#if 0
-    static void draw_oriented_rect (const Rectf& arect)
-    {
-        vec2 corners[] = { arect.getUpperLeft(), arect.getUpperRight(),
-            arect.getLowerRight(), arect.getLowerLeft() };
+        mat4 m = glm::translate( vec3( position, 0 ) );
+        m *= glm::toMat4( rotation );
+        m *= glm::scale( vec3( scale, 1 ) );
+        m *= glm::translate( vec3( -area.getSize() / 2, 0 ) );
         
-        for(unsigned index = 0; index < 4; index++)
-        {
-            unsigned index_to = (index+1)%4;
-            gl::drawLine( corners[index], corners[index_to]);
-        }
+        return m;
     }
-    
-    static void draw_oriented_rect (const std::vector<Point2f>& corners)
-    {
-        for(unsigned index = 0; index < corners.size(); index++)
-        {
-            unsigned index_to = (index+1)%(corners.size());
-            gl::drawLine( fromOcv(corners[index]), fromOcv(corners[index_to]));
-        }
-    }
-#endif
-    
+};
 
+
+class affineRectangle {
+public:
+    
+    affineRectangle () {}
+    affineRectangle (const Area& image_bounds, const ivec2& screen_position);
+    
+    void draw (const Area& display_bounds);
+
+    void mouseDown( const vec2& event_pos );
+    void mouseUp( );
+    void mouseDrag( const vec2& event_pos );
+    void resize( const vec2 change );
+    void rotate( const float change );
+    void translate ( const vec2 change );
+    bool contains ( const vec2 pos);
+    void reset (); 
+    
+    Area area () const { return mRectangle.area; }
+    const vec2&   position () const { return mRectangle.position; }
+    bool isClicked () const { return mIsClicked; }
+    vec3 mouseToWorld( const ivec2 &mouse, float z = 0 );
+    float  degrees () const;
+    float  radians () const;
+
+    const std::vector<cv::Point2f>& cornersInImage() const;
+    const cv::RotatedRect& rotatedRectInImage() const;
+    
 private:
-    Point2f map_point(Point2f pt) const;
-    void update_rect_centered_xform () const;
-    
-    vec2   mPosition;
-    vec2   mScale;
-    quat   mRotation;
-    mutable cv::Mat mXformMat;
-    mutable std::vector<vec2> mCornersWorld;
-    mutable std::vector<Point2f> mCornersWindow;
-    mutable Point2f mWindowCenter;
-    mutable vec2 mWorldCenter;
-    mutable RotatedRect mCvRotatedRect;
-    mutable glm::mat4 mWorldTransform;
-    
-    mutable RectMapping mScreen2Normal;
-    mutable std::atomic<bool> m_dirty;
+    EditableRect   mRectangle;
+    ivec2          mMouseInitial;
+    EditableRect   mRectangleInitial;
+    Area           mInitialArea;
+    ivec2          mInitialScreenPos;
+    bool           mIsClicked;
+    Rectf          mImageRect;
+    std::vector<vec2> mCornersDisplay;
+    std::vector<cv::Point2f> mCornersImage;
+    mutable cv::RotatedRect mCvRotatedRect;
     
 };
 
