88e621ebd10feaa3a2d883f865b5d2bdb93d9286
gmnamra <gmnamra@gmail.com>
2020-09-09 19:40:37 -0700

    Add basic tif stack support.
    Former-commit-id: 6cb76a245b92209d2d6c345887b32402e42158b2

---
+++ Visible/src/tiffio_wrapper.cpp
@@ -0,0 +1,176 @@
+/*
+ * tiffio_wrapper.cpp
+ *
+ *  Created on: May 8, 2015
+ *      Author: Fabian Tschopp
+ */
+
+
+// Configure LibTIFF support.
+// (http://www.libtiff.org)
+//
+// Define 'cimg_use_tiff' to enable LibTIFF support.
+//
+// TIFF library may be used to get a native support of '.tif' files.
+// (see methods 'CImg[List]<T>::{load,save}_tiff()').
+
+#define cimg_use_tiff
+
+#ifdef cimg_use_tiff
+extern "C" {
+#define uint64 uint64_hack_
+#define int64 int64_hack_
+#include "tiffio.h"
+#undef uint64
+#undef int64
+}
+#endif
+
+#include "tiffio_wrapper.hpp"
+#include <iostream>
+
+void SaveTiff(std::vector<cv::Mat> image_stack, std::string file) {
+
+  const char* filec = file.c_str();
+
+  TIFF* tif = TIFFOpen(filec, "w");
+
+  if (tif) {
+
+    for (unsigned int i = 0; i < image_stack.size(); ++i) {
+      int imagewidth = image_stack[i].cols;
+      int imageheight = image_stack[i].rows;
+      int nr_channels = image_stack[i].channels();
+      bool fp32 = (image_stack[i].type() == CV_32FC1);
+
+      unsigned char buf[imagewidth
+          * (nr_channels == 3 ?
+              sizeof(uint32) : (fp32 ? sizeof(float) : sizeof(uchar)))];
+      void* raster = &buf;
+      TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, imagewidth);
+      TIFFSetField(tif, TIFFTAG_IMAGELENGTH, imageheight);
+      TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, fp32 ? 32 : 8);
+      TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT,
+                   fp32 ? SAMPLEFORMAT_IEEEFP : SAMPLEFORMAT_UINT);
+      TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, nr_channels);
+      TIFFSetField(tif, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
+      TIFFSetField(tif, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
+      TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, nr_channels == 3 ? PHOTOMETRIC_RGB : PHOTOMETRIC_MINISBLACK);
+      TIFFSetField(tif, TIFFTAG_SUBFILETYPE, FILETYPE_PAGE);
+      TIFFSetField(tif, TIFFTAG_PAGENUMBER, i, image_stack.size());
+
+      cv::Mat image = image_stack[i];
+
+      switch (nr_channels) {
+        case 1: {
+          if (fp32) {
+            for (int y = 0; y < imageheight; ++y) {
+#pragma omp parallel for
+              for (int x = 0; x < imagewidth; ++x) {
+                ((float*) (raster))[x] = image.at<float>(y, x);
+              }
+              TIFFWriteScanline(tif, raster, y, 0);
+            }
+          } else {
+            for (int y = 0; y < imageheight; ++y) {
+#pragma omp parallel for
+              for (int x = 0; x < imagewidth; ++x) {
+                ((uchar*) (raster))[x] = image.at<uchar>(y, x);
+              }
+              TIFFWriteScanline(tif, raster, y, 0);
+            }
+          }
+        }
+          break;
+        case 3:
+        default: {
+          for (int y = 0; y < imageheight; ++y) {
+#pragma omp parallel for
+            for (int x = 0; x < imagewidth; ++x) {
+              ((uint32*) (raster))[x] = image.at<cv::Vec3b>(y, x)[2] << 16
+                  | image.at<cv::Vec3b>(y, x)[1] << 8
+                  | image.at<cv::Vec3b>(y, x)[0] << 0;
+            }
+            TIFFWriteScanline(tif, raster, y, 0);
+          }
+        }
+          break;
+      }
+      TIFFWriteDirectory(tif);
+    }
+  }
+
+  TIFFClose(tif);
+}
+
+std::vector<cv::Mat> LoadTiff(std::string file, int nr_channels) {
+
+  std::vector<cv::Mat> image_stack;
+
+  const char* filec = file.c_str();
+
+  TIFF* tif = TIFFOpen(filec, "r");
+
+  if (tif) {
+
+    unsigned int imagewidth, imageheight;
+    int dirs = 0;
+
+    TIFFSetDirectory(tif, 0);
+
+    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &imagewidth);
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &imageheight);
+
+    uint32* raster = (uint32 *) _TIFFmalloc(
+        imagewidth * imageheight * sizeof(uint32));
+
+    do {
+      ++dirs;
+    } while (TIFFReadDirectory(tif));
+
+    for (int n = 0; n < dirs; ++n) {
+      TIFFSetDirectory(tif, n);
+
+      cv::Mat image(imageheight, imagewidth, CV_8UC(nr_channels));
+      TIFFReadRGBAImageOriented(tif, imagewidth, imageheight, raster,
+      ORIENTATION_TOPLEFT);
+
+      switch (nr_channels) {
+        case 1: {
+#pragma omp parallel for
+          for (unsigned int y = 0; y < imageheight; ++y) {
+            for (unsigned int x = 0; x < imagewidth; ++x) {
+              image.at<uchar>(y, x) = raster[x + y * imagewidth] & 0xFF;
+            }
+          }
+        }
+          break;
+        case 3:
+        default: {
+#pragma omp parallel for
+          for (unsigned int y = 0; y < imageheight; ++y) {
+            for (unsigned int x = 0; x < imagewidth; ++x) {
+              image.at<cv::Vec3b>(y, x)[0] = (raster[x + y * imagewidth] >> 0)
+                  & 0xFF;
+              image.at<cv::Vec3b>(y, x)[1] = (raster[x + y * imagewidth] >> 8)
+                  & 0xFF;
+              image.at<cv::Vec3b>(y, x)[2] = (raster[x + y * imagewidth] >> 16)
+                  & 0xFF;
+            }
+          }
+        }
+          break;
+      }
+
+      image_stack.push_back(image);
+    }
+
+    _TIFFfree(raster);
+
+  }
+
+  TIFFClose(tif);
+  return image_stack;
+}
+
+
